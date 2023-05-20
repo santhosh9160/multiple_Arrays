cb9444c93d97093245321e35e218fe0bd4874c07
santhosh <santhosh1995b@gmail.com>
2023-05-20 16:11:43 +0000

    multiple Arrays

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,7 @@
+export let myArray = ["camel", 265, true, "5.6"];
+
+export let bulb = { watts: 10, type: "LED" };
+
+export let multiplyByFour = function multiplyByFour(num) {
+  return num * 4;
+};
